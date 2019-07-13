681a8a41b8dc7d8b32b81d6bb6d621273c8e0174
Rajwinder Singhbal <Rajwinder Singhbal>
2019-07-13 15:21:22 +0800

    init commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -57,9 +57,9 @@ export default function SignIn() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
+        
+        <img src={process.env.PUBLIC_URL + '/images/logo.png'}/>
+ 
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
